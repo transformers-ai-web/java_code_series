0d68484668b56628fcc37f80187b49f660163a1a
Mansi Jain <41909232+transformers-ai-web@users.noreply.github.com>
2026-01-05 00:09:02 +0530

    Add files via upload

---
+++ diameterOfBinaryTree.java
@@ -0,0 +1,40 @@
+// try submitting this on leetcode, here's the link for the problem
+// https://leetcode.com/problems/diameter-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int diameter = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        // Update diameter at this node
+        diameter = Math.max(diameter, leftHeight + rightHeight);
+
+        // Return height of subtree
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
